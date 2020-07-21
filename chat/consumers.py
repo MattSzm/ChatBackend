2747a9a2fb563262fd7647e29eac62a031988553
MattSzm <mateuszszmal@gmail.com>
2020-07-21 23:49:42 +0200

    I stay with async_to_sync instead of making my app async, because of handling with message model

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,53 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+import chat.models
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
 
 class ChatConsumer(WebsocketConsumer):
+    def fetch_messages(self, data):
+        messages_15 = chat.models.Message.last_15_messages()
+        content = {
+            'messages': self.messages_to_json(messages_15)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        author_username = data['from']
+        author = UserModel.objects.filter(username=author_username)[0]
+        #TODO: change username to the ID! /work with ids
+
+        new_message = chat.models.Message.objects.create(
+            author=author,
+            content=data['message']
+        )
+        content = {
+            'message': self.message_to_json(new_message),
+            'command': 'new_message'
+        }
+        self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        output = []
+        for message in messages:
+            output.append(self.message_to_json(message))
+        return output
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'time_stamp': str(message.time_stamp)
+        }
+
+    #pick a action
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
@@ -13,18 +58,18 @@ class ChatConsumer(WebsocketConsumer):
         )
         self.accept()
 
-
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
 
+    #the new one
+    def send_chat_message(self, message):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -33,9 +78,9 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
     def chat_message(self, event):
         message = event['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(message))
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+UserModel = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(UserModel,
+                               related_name='author_messages',
+                               on_delete=models.CASCADE)
+    content = models.TextField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_15_messages_till_now(self):
+        return Message.objects.filter(timeStamp__lte=self.time_stamp)\
+            .order_by('-time_stamp')[:15 ]
+
+    def last_15_messages(self):
+        return Message.objects.order_by('-time_stamp').all()[:15]
